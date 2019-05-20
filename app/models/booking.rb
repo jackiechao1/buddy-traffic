4f57389070b3b0bed483d23a184f7ee788f6801c
Jackie Chao <jackie.chao1@hotmail.com>
2019-05-20 16:46:08 -0400

    added validations and associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
+  belongs_to :buddy
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :status, presence: true
 end
