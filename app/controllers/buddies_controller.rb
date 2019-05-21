877a189f9077512146c1e7720aa9c52bd4bb0ca5
Jackie Chao <jackie.chao1@hotmail.com>
2019-05-21 10:43:33 -0400

    save

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -14,7 +14,7 @@ class BuddiesController < ApplicationController
   def create
     @buddy = Buddy.new(buddy_params)
     @buddy.user = current_user
-    if @buddy.save
+    if @buddy.save!
       redirect_to buddy_path(@buddy)
     else
       render :new
