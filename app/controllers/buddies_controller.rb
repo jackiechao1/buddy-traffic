689da51a792b9ac9557d939f23a4e958b5f2aae3
Jackie Chao <jackie.chao1@hotmail.com>
2019-05-21 10:38:12 -0400

    added views and controller methods

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -1,3 +1,46 @@
 class BuddiesController < ApplicationController
+  def index
+    @buddies = Buddy.all
+  end
 
+  def show
+    @buddy = Buddy.find(params[:id])
+  end
+
+  def new
+    @buddy = Buddy.new
+  end
+
+  def create
+    @buddy = Buddy.new(buddy_params)
+    @buddy.user = current_user
+    if @buddy.save
+      redirect_to buddy_path(@buddy)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @buddy = Buddy.find(params[:id])
+    @buddy.delete
+    redirect_to buddies_path
+  end
+
+  def edit
+    @buddy = Buddy.find(params[:id])
+  end
+
+  def update
+    @buddy = Buddy.find(params[:id])
+    if @buddy.update(buddy_params)
+      redirect_to buddy_path(@buddy)
+    else
+      render :edit
+    end
+  end
+
+  def buddy_params
+    params.require(:buddy).permit(:name, :location, :sex, :start_available_time, :end_available_time, :picture, :description, :user, :age)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Buddy.destroy_all
+
+a = Buddy.create(name: 'robert', location: 'montreal', age: 35, sex: 'female', description: 'Weird name for a woman', start_available_time: Date.new , end_available_time: Date.new, user_id: 1)
+
+b = Buddy.create(name: 'pop', location: 'montreal', age: 35, sex: 'female', description: 'Weird name for a woman', start_available_time: Date.new , end_available_time: Date.new, user_id: 1)
+
