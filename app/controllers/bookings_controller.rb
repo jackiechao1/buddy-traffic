75daf5005efd750fdc3f772e28dde10d95191b94
Jackie Chao <jackie.chao1@hotmail.com>
2019-05-21 13:24:35 -0400

    added destroy in booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,6 +16,12 @@ class BookingsController < ApplicationController
     end
 	end
 
+	def destroy
+		@booking = Booking.find(params[:id])
+		@booking.delete
+		redirect_to bookings_path
+	end
+
 	private
 
 	def booking_params
