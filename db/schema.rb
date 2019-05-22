2b44a66312b2c224af50961b44a786706e5c512e
Jackie Chao <jackie.chao1@hotmail.com>
2019-05-22 13:17:42 -0400

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_05_22_140354) do
-
 ActiveRecord::Schema.define(version: 2019_05_22_140851) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,11 +40,9 @@ ActiveRecord::Schema.define(version: 2019_05_22_140851) do
     t.date "end_available_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
+    t.integer "price"
     t.float "latitude"
     t.float "longitude"
-
-    t.integer "price"
     t.index ["user_id"], name: "index_buddies_on_user_id"
   end
 
