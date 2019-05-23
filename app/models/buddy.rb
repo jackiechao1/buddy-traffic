d7b45ac7db233117ee4ceab2bd3e32e1e3374775
Jackie Chao <jackie.chao1@hotmail.com>
2019-05-23 11:37:04 -0400

    changed the model and the form

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -3,10 +3,8 @@ class Buddy < ApplicationRecord
   has_many :bookings, dependent: :destroy
   mount_uploader :picture, PictureUploader
   validates :name, presence: true
-  validates :age, presence: true
-  validates :sex, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :skills, presence: true
   validates :location, presence: true
   validates :start_available_time, presence: true
   validates :end_available_time, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_140851) do
+ActiveRecord::Schema.define(version: 2019_05_23_144838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(version: 2019_05_22_140851) do
     t.string "name"
     t.bigint "user_id"
     t.string "location"
-    t.integer "age"
-    t.string "sex"
     t.string "description"
     t.string "picture"
     t.date "start_available_time"
@@ -42,7 +40,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_140851) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
-    t.integer "price"
+    t.string "skills", default: [], array: true
     t.index ["user_id"], name: "index_buddies_on_user_id"
   end
 
@@ -52,10 +50,6 @@ ActiveRecord::Schema.define(version: 2019_05_22_140851) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
